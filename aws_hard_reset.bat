98a20e22f7e486a17d2584286847f61206028116
Brian Bornino <brianbornino@gmail.com>
2026-03-31 14:05:25 -0700

    Correctly encode UTF-8 and defer script loading

---
+++ aws_hard_reset.bat
@@ -0,0 +1,2 @@
+@REM aws_hard_reset.bat
+aws s3 rm s3://info.bornino.net --recursive
--- aws_sync.bat
+++ aws_sync.bat
@@ -1,1 +1,3 @@
-aws s3 sync . s3://info.bornino.net --delete --exclude ".git/*" --content-type "text/html; charset=utf-8"
+@REM aws_sync.
+@REM If Necessary, run aws_hard_reset.bat then aws_sync.bat, then aws_invalidate.bat
+aws s3 sync . s3://info.bornino.net --delete --exclude ".git/*" --exclude "*.bat" --exclude ".vscode/*" --cache-control "no-cache, no-store, must-revalidate"
